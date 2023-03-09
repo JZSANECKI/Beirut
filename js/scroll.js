f55f8674b4918d798b3b560218d72ab1d39e7431
Jakub Sanecki <j.z.sanecki@gmail.com>
2023-03-09 18:25:00 +0100

    ogarnięty skrypt menu

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,6 @@
-const section = document.querySelectorAll('section')
+const section = document.querySelectorAll('.section')
 const navLink = document.querySelectorAll('.nav2 a')
+const navLinkM = document.querySelectorAll('.nav2-mobile a')
 const homeBtn = document.querySelector('.home-btn')
 
 
@@ -12,15 +13,25 @@ let offset = sec.offsetTop - 600
 let height = sec.offsetHeight
 let id = sec.getAttribute('id')
 
-if (top >=offset && top < offset + height){
+if (top >=offset && top < offset + height)
+{
     navLink.forEach(link =>{
         link.classList.remove('nav2-active')
+        console.log(id)
         document.querySelector('.nav2 a[href*='+ id +']').classList.add('nav2-active')
     })
+
+    navLinkM.forEach(link =>{
+        link.classList.remove('nav2-mobile-active')
+        document.querySelector('.nav2-mobile a[href*='+ id +']').classList.add('nav2-mobile-active')
+    })
+
 }
 
 
 })
+
+
 
 if (scrollY >= 300){
     homeBtn.classList.add('home-btn-up')
